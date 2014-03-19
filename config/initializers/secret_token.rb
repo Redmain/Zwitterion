ceef27171529488c9cd738848bb1b95b03924e1b
Redmain <hariukoleg@gmail.com>
2014-03-19 20:47:36 +0200

    Add project in repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zwitterion::Application.config.secret_key_base = 'aafe778e2a4ea9c58888ae3097686a6242d3a9191cbc3a997ff85f3062afe4ff460d58107d7d14601bda9d3520a791ea866051484862a545f4ca7b1ff47809dd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Zwitterion::Application.config.session_store :cookie_store, key: '_Zwitterion_session'
