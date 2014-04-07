afa6f4e1ed7b93afbcbd09880fd61c7b96b20a4d
Redmain <hariukoleg@gmail.com>
2014-04-07 17:37:17 +0300

    DELETE: delete migrates

--- db/migrate/20140324154323_add_new_columns_and_remove_one.rb
+++
@@ -1,7 +0,0 @@
-class AddNewColumnsAndRemoveOne < ActiveRecord::Migration
-  def change
-  	add_column :questions, :owner_id, :integer
-    add_column :questions, :test_list_id, :integer
-    remove_column :questions, :test_id, :integer
-  end
-end
